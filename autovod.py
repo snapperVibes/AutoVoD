df768c77475eaf97c034c8700425966d93af1d55
Snapper Vibes <learningwithsnapper@gmail.com>
2021-07-17 19:13:30 -0400

    Clarified Excel datetime issue; overrode MoviePy stdout

--- autovod.py
+++ autovod.py
@@ -1,4 +1,8 @@
+import datetime
+from contextlib import redirect_stdout
 from dataclasses import dataclass
+from unittest.mock import MagicMock
+
 import click
 from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
 from openpyxl import load_workbook
@@ -12,7 +16,11 @@ class Match:
     _end: str
 
     def _to_time(self, time):
-        h, m, s = time.split(":")
+        if isinstance(time, datetime.time):
+            raise TypeError(
+                "Oops! It looks like the Excel spreadsheet views your time as an actual 'time' rather than text. "
+            )
+        h, m, s = map(int, time.split(":"))
         return (h * 3600) + (m * 60) + s
 
     @property
@@ -41,9 +49,14 @@ def cli(data, vod):
 
     # Split the videos
     for match in matches:
-        ffmpeg_extract_subclip(
-            vod, match.start_time, match.end_time, targetname=f"{match.title}.mp4"
-        )
+        title = f"{match.title}.mp4"
+        print(f"Exporting '{title}'")
+        # By default, Moviepy outputs 3 lines of text for every clip.
+        # This is a hack to get rid of them (so we can replace it with our own)
+        with redirect_stdout(MagicMock()):
+            ffmpeg_extract_subclip(
+                vod, match.start_time, match.end_time, targetname=title
+            )
 
 
 if __name__ == "__main__":
